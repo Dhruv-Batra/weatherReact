990521a9f0155ff37a500f9bd697c28153a88c5d
Dhruv Batra <dhruvbatra2@gmail.com>
2021-05-02 05:16:02 -0400

    done for the night

---
+++ Documents/Launch/weather/src/App.js
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import './App.css';
+import Input from './Input';
+
+import React, { useEffect, useState } from "react";
+
+function App() {
+  
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>Weather</h2>
+        <div class="grid-container">
+          <img src={logo} className="App-logo grid-item" alt="logo"/>
+          <img src={logo} className="App-logo grid-item" alt="logo"/>
+          <img src={logo} className="App-logo grid-item" alt="logo"/>
+          <img src={logo} className="App-logo grid-item" alt="logo"/>
+          <img src={logo} className="App-logo grid-item" alt="logo"/>
+        </div>
+      </header>
+      <div class="App-body">
+      <br></br>
+
+      <Input/>
+
+      </div>  
+    </div>
+  );
+}
+
+export default App;
---
+++ Documents/Launch/weather/src/Hourly.js
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from "react";
+const API_KEY = process.env.REACT_APP_api_key;
+
+export default function Hourly({long,lati}){
+
+    const [hour, setHour] = useState(null);
+
+
+    useEffect(() => {
+        const url = new URL("https://api.openweathermap.org/data/2.5/onecall");
+        url.searchParams.append("appid", API_KEY);
+        url.searchParams.append("lon", long);
+        url.searchParams.append("lat", lati);
+        url.searchParams.append("units", "imperial");
+        url.searchParams.append("exclude", "current,minutely,daily,alerts");
+        fetch(url)
+        .then((resp) => {
+            return resp.json();
+        })
+        .then((obj) => {
+            // also important to check html error codes
+            // 200 means no errors
+            if (obj.cod === 200) {
+            setHour(obj);
+            } else {
+            setHour(false);
+            }
+        });
+    }, []);
+
+
+
+    //<pre>{JSON.stringify(weather, undefined, 4)}</pre>
+
+    //the commented out part here doesn't return anything when button is clicked
+    /*return(hour&&(
+        <div>
+            <pre>{JSON.stringify(hour, undefined, 4)}</pre>
+        </div>
+    ));*/
+
+    return(
+        <div>
+            <p>INSERT HOURLY FORECASE HERE</p>
+            <pre>{JSON.stringify(hour, undefined, 4)}</pre>
+        </div>
+    );
+
+};
